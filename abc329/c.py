e2fd3e65b090a766867542044f92f59baa0fd760
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2023-11-21 16:09:58 +0900

    abc329 c

--- abc329/c.py
+++ abc329/c.py
@@ -1,8 +1,17 @@
 def main():
     N = int(input())
     S = input()
-    counts = set(list(S))
-    
+    i = 0
+    counts = {}
+    while i < N:
+        c = 1
+        while i + 1 < N and S[i] == S[i + 1]:
+            c += 1
+            i += 1
+        if counts.get(S[i], 0) < c:
+            counts[S[i]] = c
+        i += 1
+    print(sum(counts.values()))
 
 
 main()
