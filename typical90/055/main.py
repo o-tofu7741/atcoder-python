e803ca65cf6cb4398827bf237d25430c1af680c1
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2023-12-04 01:01:11 +0900

    典型055

---
+++ typical90/055/main.py
@@ -0,0 +1,30 @@
+# import bisect
+# import collections
+# import copy
+# import heapq
+# import itertools
+# import math
+# import string
+import sys
+
+# import numpy
+
+
+def inp_i():
+    return int(sys.stdin.readline().rstrip())
+
+
+def inp_li():
+    return list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def inp_s():
+    return sys.stdin.readline().rstrip()
+
+
+def inp_ls():
+    return list(sys.stdin.readline().rstrip().split())
+
+
+N = inp_i()
+A = [inp_li() for _ in range(N)]
