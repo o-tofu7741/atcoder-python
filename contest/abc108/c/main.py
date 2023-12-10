60e36d4ad5efe29bb7f86be2858a19117dfb4414
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2023-12-10 20:58:06 +0900

    abc108

---
+++ contest/abc108/c/main.py
@@ -0,0 +1,37 @@
+# import bisect
+# import collections
+# import copy
+# import heapq
+# import itertools
+# import math
+# import string
+import sys
+
+# import numpy
+
+
+def inp_i():
+    return int(sys.stdin.readline().rstrip())
+
+
+def inp_li():
+    return list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def inp_s():
+    return sys.stdin.readline().rstrip()
+
+
+def inp_ls():
+    return list(sys.stdin.readline().rstrip().split())
+
+
+def main():
+    N, K = inp_li()
+    ans = sum(map(lambda x: 1 if x % K == 0 else 0, range(1, N + 1))) ** 3
+    
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
