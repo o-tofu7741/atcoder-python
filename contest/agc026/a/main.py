2f2b491ab97d38c6c5a5d45ba3fb779dfb33b2c6
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2024-01-31 12:09:02 +0900

    agc026a

---
+++ contest/agc026/a/main.py
@@ -0,0 +1,39 @@
+# import bisect
+# import collections
+# import copy
+# import heapq
+import itertools
+
+# import math
+# import string
+import sys
+
+# import numpy
+
+
+def inp_i():
+    return int(sys.stdin.readline().rstrip())
+
+
+def inp_li():
+    return list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def inp_s():
+    return sys.stdin.readline().rstrip()
+
+
+def inp_ls():
+    return list(sys.stdin.readline().rstrip().split())
+
+
+def main():
+    N = inp_i()
+    ans = 0
+    for i, v in itertools.groupby(inp_li()):
+        ans += len(list(v)) // 2
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
