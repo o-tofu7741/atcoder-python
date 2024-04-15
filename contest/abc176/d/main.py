8ed9703ad64fd8303f86920929603e4e63079357
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2024-04-15 10:50:31 +0900

    abc176 d

--- contest/abc176/d/main.py
+++ contest/abc176/d/main.py
@@ -30,7 +30,43 @@ def inp_ls():
 
 
 def main():
-    
+    h, w = inp_li()
+    ch, cw = inp_li()
+    dh, dw = inp_li()
+    s = [list(inp_s()) for _ in range(h)]
+
+    deq = collections.deque([])
+    deq.append((cw - 1, ch - 1))
+    pt_walk = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+    pt_warp = list(itertools.product([-2, -1, 0, 1, 2], repeat=2))
+    dist = [[INF] * w for _ in range(h)]
+    dist[ch - 1][cw - 1] = 0
+
+    while deq:
+        x, y = deq.popleft()
+        for px, py in pt_walk:
+            nx = x + px
+            ny = y + py
+            if (
+                0 <= nx < w
+                and 0 <= ny < h
+                and s[ny][nx] == "."
+                and dist[ny][nx] > dist[y][x]
+            ):
+                dist[ny][nx] = dist[y][x]
+                deq.appendleft((nx, ny))
+        for px, py in pt_warp:
+            nx = x + px
+            ny = y + py
+            if (
+                0 <= nx < w
+                and 0 <= ny < h
+                and s[ny][nx] == "."
+                and dist[ny][nx] > dist[y][x] + 1
+            ):
+                dist[ny][nx] = dist[y][x] + 1
+                deq.append((nx, ny))
+    print(dist[dh - 1][dw - 1] if dist[dh - 1][dw - 1] != INF else -1)
 
 
 if __name__ == "__main__":
